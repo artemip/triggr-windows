c4c8fcef18e0ff72366e4e576dca54058f5c5546
Artem Pasyechnyk <artem.i.p@gmail.com>
2012-09-16 14:57:16 -0400

    Create and maintain socket connection; Lower volume to 10%, or reset to previous value when necessary

---
+++ cBridge-desktop/SocketHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace cBridge_desktop
+{
+    static class SocketHelper
+    {
+        public static Socket ConnectSocket(string server, int port)
+        {
+            Socket s = null;
+            IPHostEntry hostEntry = null;
+
+            // Get host related information.
+            hostEntry = Dns.GetHostEntry(server);
+
+            // Loop through the AddressList to obtain the supported AddressFamily. This is to avoid 
+            // an exception that occurs when the host IP Address is not compatible with the address family 
+            // (typical in the IPv6 case). 
+            foreach (IPAddress address in hostEntry.AddressList)
+            {
+                IPEndPoint ipe = new IPEndPoint(address, port);
+                Socket tempSocket =
+                    new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                tempSocket.Connect(ipe);
+
+                if (tempSocket.Connected)
+                {
+                    s = tempSocket;
+                    break;
+                }
+            }
+            return s;
+        }
+    }
+}
